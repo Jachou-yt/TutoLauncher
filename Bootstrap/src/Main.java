3588602d7258ed28d169be70afc1d1f09228bfef
Matisse <matisse.martayan@gmail.com>
2024-10-23 14:46:20 +0200

    Sortie de la vidéo

--- Bootstrap/src/Main.java
+++ Bootstrap/src/Main.java
@@ -7,6 +7,7 @@ import fr.theshark34.openlauncherlib.external.ExternalLauncher;
 import fr.theshark34.openlauncherlib.minecraft.*;
 import fr.theshark34.openlauncherlib.util.CrashReporter;
 import fr.theshark34.openlauncherlib.util.SplashScreen;
+import fr.theshark34.openlauncherlib.util.explorer.FileList;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -15,6 +16,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 
@@ -42,12 +44,15 @@ public class Main {
 
     public static void doUpdate() throws Exception {
         FlowUpdater flowUpdater = new FlowUpdater.FlowUpdaterBuilder().withExternalFiles(ExternalFile.getExternalFilesFromJson("https://raw.githubusercontent.com/Jachou-yt/TutoLauncher/refs/heads/master/launcher_update/launcher.json")).build();
-        flowUpdater.update(path);
+        flowUpdater.update(Paths.get(path + "/launchers"));
     }
 
     public static void launchLauncher() throws LaunchException {
         ClasspathConstructor constructor = new ClasspathConstructor();
-        constructor.add((List<Path>) new File(launcherFile, "TutoLauncher.jar"));
+
+        FileList fileList = new FileList();
+        fileList.add(new File(launcherFile, "TutoLauncher.jar").toPath());
+        constructor.add(fileList);
 
         ExternalLaunchProfile profile = new ExternalLaunchProfile("fr.jachou.tutolauncher.Frame", constructor.make());
         ExternalLauncher launcher = new ExternalLauncher(profile);
